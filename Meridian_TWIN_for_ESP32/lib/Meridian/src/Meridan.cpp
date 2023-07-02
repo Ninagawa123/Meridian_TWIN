16bc70e0713b3975ceb2d6886b1a713730662bc1
Ninagawa123 <8329123+Ninagawa123@users.noreply.github.com>
2023-07-02 18:24:56 +0900

    Update Meridan.cpp

--- Meridian_TWIN_for_ESP32/lib/Meridian/src/Meridan.cpp
+++ Meridian_TWIN_for_ESP32/lib/Meridian/src/Meridan.cpp
@@ -11,7 +11,7 @@ ARDUINO_ROBOTICS_ESPTEENSY_MERIDIAN_NAMESPACE_BEGIN
  *         MIT license.
  *         Detalis: https://github.com/Ninagawa123/Meridian_TWIN
  *  @author Izumi Ninagawa
- *  @date 2023/06/28
+ *  @date 2023/07/02
  *  @version 1.0.0
  *  @copyright &copy; Izumi Ninagawa & Project Meridian
  */
@@ -335,14 +335,14 @@ void Meridian::print_esp_hello_start(String version, String serial_pc_bps, Strin
  * @param[in] fixed_ip_addr Fixed Wi-Fi IP of this device. (If mode_fixed_ip is True)
  * @param[in] mode_fixed_ip Use fixed Wi-Fi IP or Not.
  */
-void Meridian::print_esp_hello_ip(String wifi_send_ip, IPAddress wifi_localip, String fixed_ip_addr, bool mode_fixed_ip)
+void Meridian::print_esp_hello_ip(String wifi_send_ip, String wifi_localip, String fixed_ip_addr, bool mode_fixed_ip)
 {
-    Serial.println("WiFi successfully connected.");                          // WiFi接続完了通知
-    Serial.println("PC's IP address target is  => " + String(wifi_send_ip)); // 送信先PCのIPアドレスの表示
+    Serial.println("WiFi successfully connected.");                  // WiFi接続完了通知
+    Serial.println("PC's IP address target is  => " + wifi_send_ip); // 送信先PCのIPアドレスの表示
 
     if (mode_fixed_ip)
     {
-        Serial.println("ESP32's IP address is  => " + String(fixed_ip_addr) + " (*Fixed)"); // ESP32自身のIPアドレスの表示
+        Serial.println("ESP32's IP address is  => " + fixed_ip_addr + " (*Fixed)"); // ESP32自身のIPアドレスの表示
     }
     else
     {
