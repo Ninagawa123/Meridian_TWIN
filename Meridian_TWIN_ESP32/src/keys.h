3301fa02f7240331aa5b4dad2bf062133cbb9530
Ninagawa123 <8329123+Ninagawa123@users.noreply.github.com>
2025-08-02 14:28:44 +0900

    Update keys.h
    Typo

--- Meridian_TWIN_ESP32/src/keys.h
+++ Meridian_TWIN_ESP32/src/keys.h
@@ -6,7 +6,7 @@
 #define WIFI_AP_PASS "xxxxxxxx"       // アクセスポイントのパスワード
 #define WIFI_SEND_IP "192.168.1.xx"   // 送り先のPCのIPアドレス（PCのIPアドレスを調べておく）
 #define UDP_SEND_PORT 22222         // 送り先のポート番号
-#define UDP_RESV_PORT 22224         // このESP32のポート番号
+#define UDP_RECV_PORT 22224         // このESP32のポート番号
 
 // ESP32のIPアドレスを固定する場合は下記の4項目を設定
 #define MODE_FIXED_IP 0 // IPアドレスを固定するか（0:NO, 1:YES）
